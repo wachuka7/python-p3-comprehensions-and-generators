aacd8e8c852c7f4981fa66aa840ca68b7c1bc8c7
wachuka7 <rachaelwachuka7@gmail.com>
2024-03-03 09:02:35 +0300

    feat:add functions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers= [num for num in num_list if num%2==0]
+    return(even_numbers)
+
 
 def make_exclamation(sentence_list):
-    pass
+    sentence= [sentence_list + '!' for sentence_list in sentence_list]
+    return(sentence)
